1b4733a087d0e48dae208ad0042d02f9ee26d326
Fengxing <272355332@qq.com>
2018-11-20 18:06:55 +0800

    input组件

---
+++ projects/myaccountMobile/src/components/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+    <i :class="className"></i>
+</template>
+
+<script>
+export default {
+    name: 'MobileIcon',
+
+    componentName: 'MobileIcon',
+
+    props: {
+        type: String,
+        size: String,
+    },
+
+    computed: {
+        className () {
+            return `mobile-icon mobile-icon--${this.type.replace(/_/g, '-')} mobile-icon--${this.size ? this.size : 'normal'}`;
+        }
+    }
+};
+</script>
--- projects/myaccountMobile/src/components/InputComponent.vue
+++ projects/myaccountMobile/src/components/InputComponent.vue
@@ -119,10 +119,12 @@
             />
         </div>
         <div class="mobile-input__right">
-            <icon
+            <Icon
                 type="clear"
+                v-show="showClear && currentValue !== '' && !readonly && !diabled && isFocus"
+                @click.native="clear"
             >
-            </icon>
+            </Icon>
         </div>
         <div>
 
@@ -130,10 +132,16 @@
     </div>
 </template>
 <script>
+import Icon from './Icon.vue';
+
 export default {
     name: 'MobileInput',
 
     componentName: 'MobileInput',
+
+    components: {
+        Icon,
+    },
 
     props: {
         height: {
@@ -166,12 +174,54 @@ export default {
         name: String,
         placeholder: {
             type: String,
-            default: '',
+            default: ''
         },
         readonly: Boolean,
         disabled: Boolean,
-
+        required: {
+            type: Boolean,
+            default: false
+        },
+        requiredMessage: String,
+        rules: {
+            type: Array,
+            default: []
+        },
+        // rules demo
+        //
+        // [
+        //     {
+        //         type: 'required',
+        //         message: '此字段为必填'
+        //     },
+        //     {
+        //         type: 'regex',
+        //         value: /\d[1-9]/,
+        //         message: '此字段不符合正则表达式'
+        //     },
+        //     {
+        //         type: 'min-length',
+        //         value: 6,
+        //         message: '此字段最少6个字符'
+        //     },
+        //     {
+        //         type: 'max-length',
+        //         value: 10,
+        //         message: '此字段最多10个字符'
+        //     },
+        // ]
     },
+
+    data () {
+        return {
+            isFocus: false,
+            currentValue: '',
+            valid: true,
+            errors: {},
+            firstError: null,
+        }
+    },
+
     computed: {
         pattern () {
             if (this.keyboard === 'number' || this.isType === 'china-mobile') {
@@ -181,8 +231,44 @@ export default {
     },
     methods: {
         focusHandler ($event) {},
-        blurHandler ($event) {},
+        blurHandler ($event) {
+            this.validate();
+            this.isFocus = false;
+            this.$emit('on-blur', this.currentValue, $event)
+        },
         keyupHandler ($event) {},
+        clear () {},
+        getFirstError () {
+            let key = Object.keys(this.errors)[0];
+            this.firstError = this.errors[key];
+        },
+        validate () {
+            this.errors = {};
+            if (!this.currentValue && !this.required) {
+                this.valid = true;
+                return;
+            }
+
+            if (!this.currentValue && this.required) {
+                this.valid = false
+                this.errors.required = this.requiredMessage;
+                this.getFirstError();
+                return;
+            }
+
+            for (let i = 0; i < this.rules.length; i++) {
+                if (this.rules[i].type === 'required') {
+
+                } else if (this.rules[i].type === 'regex') {
+
+                } else if (this.rules[i].type === 'min-length') {
+
+                } else if (this.rules[i].type === 'max-length') {
+
+                }
+            }
+        },
+
     },
 };
 </script>

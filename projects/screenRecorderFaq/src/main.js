e316f37f644745a4f0ac7649cca81362990bfbc1
Fengxing <272355332@qq.com>
2018-12-21 14:51:04 +0800

    加入中英文翻译

--- projects/screenRecorderFaq/src/main.js
+++ projects/screenRecorderFaq/src/main.js
@@ -1,16 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 import './styles/themes/default/index.scss';
 import App from './App.vue';
 import router from './router';
+import { getQueryValue, } from './utils/index';
+
+// import languages
+import LangEn from './lang/en.json';
+import LangZh from './lang/zh.json';
 
 Vue.config.productionTip = false;
+Vue.use(VueI18n);
+
+let lang = getQueryValue('lang') || 'en';
+
+const i18n = new VueI18n({
+    locale: lang,
+    messages: {
+        'zh': LangZh,
+        'en': LangEn,
+    },
+});
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
+    i18n,
     components: { App, },
     template: '<App/>',
 });

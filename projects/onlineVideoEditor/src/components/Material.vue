736dbc3445789c6f62c02697fdd6074f945c9385
Fengxing <272355332@qq.com>
2019-01-03 20:06:38 +0800

    material组件

--- projects/onlineVideoEditor/src/components/Material.vue
+++ projects/onlineVideoEditor/src/components/Material.vue
@@ -1,11 +1,34 @@
 <template>
     <div class="editor-material">
-        <div class="editor-material__switch">
-            <div v-for="category in categories" v-bind:key="category" @click="switchCategory(category)">
-                <p>{{ category }}</p>
+        <div class="editor-material__menu">
+            <div class="editor-material__menu-button" :class="{ 'active': activeCategory === 'materials' }" @click="loadMaterial('materials')">
+                <p>素材</p>
+            </div>
+            <div class="editor-material__menu-button" :class="{ 'active': activeCategory === 'text' }" @click="loadMaterial('text')">
+                <p>文字</p>
+            </div>
+            <div class="editor-material__menu-button" :class="{ 'active': activeCategory === 'filters' }" @click="loadMaterial('filters')">
+                <p>滤镜</p>
+            </div>
+            <div class="editor-material__menu-button" :class="{ 'active': activeCategory === 'overlays' }" @click="loadMaterial('overlays')">
+                <p>叠附</p>
+            </div>
+            <div class="editor-material__menu-button" :class="{ 'active': activeCategory === 'transitions' }" @click="loadMaterial('transitions')">
+                <p>转场</p>
+            </div>
+            <div class="editor-material__menu-button" :class="{ 'active': activeCategory === 'animations' }" @click="loadMaterial('animations')">
+                <p>动画</p>
             </div>
         </div>
-        <div class="editor-material__content"></div>
+        <div class="editor-material__content">
+            <div class="editor-material__content-materials" v-if="activeCategory === 'materials'">
+                <div v-for="material in materials">
+
+                </div>
+            </div>
+        </div>
+        <div class="fx-explorer__sash vertical" id="ExplorerSash">
+        </div>
     </div>
 </template>
 
@@ -17,14 +40,43 @@ export default {
     },
     data() {
         return {
-            categories: ['materials', 'words', 'filters', 'overlays', 'transitions', 'animations', ],
+            categories: ['materials', 'text', 'filters', 'overlays', 'transitions', 'animations', ],
+            activeCategory: 'materials',
+            materials: [{
+                id: 1,
+                cover: '',
+                type: 'image',
+                url: '',
+            }, {
+                id: 2,
+                cover: '',
+                type: 'video',
+                m3u8: null,
+            }],
         };
     },
 
     created: function() {
+        this.loadMaterial('materials');
     },
     methods: {
-        switchCategory: () => {
+
+        loadMaterial(category) {
+            this.activeCategory = category;
+            if (category === 'materials') {
+                return
+            }
+        },
+
+        switchCategory() {
+
+        },
+
+        handleOpen() {
+
+        },
+
+        handleClose() {
 
         },
     },
--- projects/onlineVideoEditor/src/main.js
+++ projects/onlineVideoEditor/src/main.js
@@ -1,16 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import { MdButton, MdContent, MdTabs, } from 'vue-material/dist/components';
-import 'vue-material/dist/vue-material.min.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import './styles/themes/default/index.scss';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-Vue.use(MdButton);
-Vue.use(MdContent);
-Vue.use(MdTabs);
+Vue.use(ElementUI, { size: 'small', });
 
 Vue.config.productionTip = false;
 

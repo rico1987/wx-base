3567fd4eb3afe6c7f472b5b9dc4ba19e96ae62f2
Rico <272355332@qq.com>
2019-04-15 13:41:50 +0800

    移除airmore和 onlinevideoeditor项目

--- projects/airmore/src/config/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-    accountApiBaseUrl: 'https://passport.aoscdn.com/api',
-    supportApiBaseUrl: 'https://support.aoscdn.com/api',
-    fileApiBaseUrl: 'https://file.aoscdn.com/api',
-    storageApiBaseUrl: 'https://storage.aoscdn.com/api',
-    timeout: 5000,
-};
--- projects/airmore/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-    language: state => state.app.language,
-    api_token: state => state.user.api_token,
-    identity_token: state => state.user.identity_token,
-    userInfo: state => state.user.userInfo,
-    licenseInfo: state => state.user.licenseInfo,
-    errorLogs: state => state.errorLog.logs,
-};
-export default getters;
